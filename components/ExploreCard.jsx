f0b3acedd6cf8230e1ea2891e90793e6476e82c9
Ronnel Aldrin Mariano <ronnelaldrinmariano@gmail.com>
2023-08-05 23:12:32 +0800

    Debug the code

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,21 +1,19 @@
-'use client';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
 
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { fadeIn } from "../utils/motion";
-
-const ExploreCard = ({id, imgUrl, title, index, active, handleClick}) => (
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
-  variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-  className={`relative ${
-    active === id? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-  } flex items-center justify-center min-w-[170px] h-[700px] transistion-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
-  onClick={() => handleClick(id)}
+    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    onClick={() => handleClick(id)}
   >
-    <img 
+    <img
       src={imgUrl}
       alt={title}
-      className="absolute w-full h-full object-cover rounder-[24px]"
+      className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
@@ -24,7 +22,7 @@ const ExploreCard = ({id, imgUrl, title, index, active, handleClick}) => (
     ) : (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
-          <img 
+          <img
             src="/headset.svg"
             alt="headset"
             className="w-1/2 h-1/2 object-contain"
