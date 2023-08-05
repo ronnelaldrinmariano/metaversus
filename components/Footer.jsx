723c82926d1bc60b5890e5872a115a1540bd0499
Ronnel Aldrin Mariano <ronnelaldrinmariano@gmail.com>
2023-08-05 23:17:17 +0800

    Debugged code

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,8 +7,8 @@ import { footerVariants } from '../utils/motion';
 const Footer = () => (
   <motion.footer
     variants={footerVariants}
-    initial='hidden'
-    whileInView='show'
+    initial="hidden"
+    whileInView="show"
     className={`${styles.paddings} py-8 relative`}
   >
     <div className="footer-gradient" />
